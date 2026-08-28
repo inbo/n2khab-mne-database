f8332cb9075f55438d22b39140e4236349e78f02
Falk Mielke <falk.mielke@inbo.be>
2026-08-28 15:35:07 +0200

    oopdb: roxygen docs test

--- R/mnmdbAuth.R
+++ R/mnmdbAuth.R
@@ -2,6 +2,14 @@
 
 # this file contains the mnmdb authentication object.
 
+#' Authentication for mnmdb via config file
+#'
+#' manage authentification storage for connections to mnm databases
+#'
+#' @details tbd
+#'
+#' @param config_file file.path to a config file to load with `configr`
+#'
 mnmdbAuth <- S7::new_class(
   name = "mnmdbAuth",
   properties = list(
