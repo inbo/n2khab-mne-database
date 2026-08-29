e8b67fff3b310ca48e6b267af33589462195422e
Falk Mielke <falk.mielke@inbo.be>
2026-08-29 09:40:12 +0200

    oopdb: bring in require_pkgs helper
    from n2khab-mne-monitoring

---
+++ R/require_pkgs.R
@@ -0,0 +1,48 @@
+#!usr/bin/env Rscript
+
+#' Check availability of required packages
+#'
+#' Takes a vector of package names and passes each name to
+#' \code{\link[base:ns-load]{requireNamespace()}};
+#' if package(s) are missing, returns an error message providing the basic
+#' \code{install.packages()} command to install them.
+#'
+#' @param pkgs A character vector of package names.
+#' @inheritParams requireNamespace
+#'
+#' @examples
+#' \dontrun{
+#'   require_pkgs(c("a", "base", "b", "magrittr"))
+#'   # not_attached <- any(devtools::loaded_packages() == "magrittr") == FALSE
+#' }
+#'
+#' @keywords internal
+require_pkgs <- function(pkgs, ...) {
+
+  # verify user input
+  assertthat::assert_that(is.character(pkgs))
+
+  the_loading_function <- \(pkg) requireNamespace(
+    pkg,
+    ...
+  )
+
+  # check availability of the package
+  available <- vapply(pkgs, the_loading_function, FUN.VALUE = TRUE)
+
+  # feedback availability
+  if (!all(available)) {
+    multiple <- sum(!available) > 1
+    stop(ifelse(multiple, "Multiple", "A"),
+         " package",
+         ifelse(multiple, "s", ""),
+         " needed for this function ",
+         ifelse(multiple, "are", "is"),
+         " missing.\nPlease install as follows: install.packages(",
+         deparse(pkgs[!available]),
+         ")",
+         call. = FALSE
+    )
+  }
+
+} # /require_pkgs
